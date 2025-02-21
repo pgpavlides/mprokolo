d11e9da3f1b3c7b7b3d083738f0ed994a624f2cc
George Pavlides <95945945+pgpavlides@users.noreply.github.com>
2025-02-21 20:00:33 +0200

    addition:new_linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,39 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+// eslint.config.mjs
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import nextConfig from "@next/eslint-plugin-next";
 
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
+export default [
+  {
+    plugins: {
+      "@next/next": nextConfig,
+    },
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module",
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    rules: {
+      ...nextConfig.configs.recommended.rules,
+      ...nextConfig.configs["core-web-vitals"].rules,
+    },
+  },
+];
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+// import { dirname } from "path";
+// import { fileURLToPath } from "url";
+// import { FlatCompat } from "@eslint/eslintrc";
 
-export default eslintConfig;
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+
+// const compat = new FlatCompat({
+//   baseDirectory: __dirname,
+// });
+
+// const eslintConfig = [...compat.extends("next/core-web-vitals")];
+
+// export default eslintConfig;
